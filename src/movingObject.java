c34929f7bc507dc8f5e8190c996fd61a00409e0e
julianthunell <123653265+julianthunell@users.noreply.github.com>
2024-03-26 14:54:35 +0100

    Game loop klar update och render + moving object

---
+++ src/movingObject.java
@@ -0,0 +1,12 @@
+public class movingObject {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    public movingObject(int x, int y, int width, int height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
